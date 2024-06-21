b56b6af03bfdacff40bd4db42544e0f6e302f532
jethrocadang <116892945+jethrocadang@users.noreply.github.com>
2024-06-21 18:24:37 +0800

    sider:[done]

--- src/app/tasks/_components/sider.tsx
+++ src/app/tasks/_components/sider.tsx
@@ -1,48 +1,25 @@
 "use client";
 
 import Link from "next/link";
-import { RiDashboardFill } from "react-icons/ri";
-import { MdTask, MdOutlineLogout } from "react-icons/md";
-import { FaUsers } from "react-icons/fa";
-import { usePathname } from "next/navigation";
 import { Card } from "@/components/ui/card";
-import CircularProgressBar from "@/components/circular-progress/circular-progress";
+import ProgressCard from "@/components/progress-card";
+import { Calendar } from "@/components/ui/calendar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const TaskSider = () => {
-  const pathname = usePathname();
-
-  const hiddenNav = ["/"];
-
-  if (hiddenNav.includes(pathname)) {
-    return null;
-  }
-
   return (
     <aside className="h-full rounded-md py-2.5">
       <Card className="flex h-full flex-col px-8 pt-16">
-        <Card>
-          <div className="flex flex-col items-center space-y-5 px-12 py-7">
-            <p className="text-lg font-black tracking-wide">Progress</p>
-            <CircularProgressBar percentage={10} />
-            <div className="flex gap-12 text-sm">
-              <div className="flex items-center gap-1.5">
-                <div className="h-2 w-2 rounded-full bg-sky-500" />
-                <div className="leading-tight">
-                  <p className="font-bold">10</p>
-                  <p className="text-xs tracking-tight">Completed</p>
-                </div>
-              </div>
-              <div className="flex items-center gap-1.5">
-                <div className="h-2 w-2 rounded-full bg-sky-500" />
-                <div className="leading-tight">
-                  <p className="font-bold">10</p>
-                  <p className="w-full text-nowrap text-xs tracking-tight">
-                    Total Tasks
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+        <div className="flex flex-col items-center mb-12 gap-3">
+          <Avatar>
+            <AvatarImage src="/avatar.jpg" />
+            <AvatarFallback>JC</AvatarFallback>
+          </Avatar>
+          <p className="tracking-normal text-sm font-semibold">Jethro Cadang</p>
+        </div>
+        <ProgressCard />
+        <Card className="mt-10">
+          <Calendar className="flex justify-center" />
         </Card>
       </Card>
     </aside>
