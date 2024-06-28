de9008b756a47423c96c76d5c4ebfb83ba760448
jethrocadang <116892945+jethrocadang@users.noreply.github.com>
2024-06-28 22:48:11 +0800

    task:[change layout]

--- src/components/task-card.tsx
+++ src/components/task-card.tsx
@@ -1,23 +1,29 @@
+"use client";
+
 import { Card } from "@/components/ui/card";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Badge } from "./ui/badge";
+import { RiMore2Fill } from "react-icons/ri";
 
 const TaskCard = () => {
   return (
     <div className="flex items-center gap-3">
       <Checkbox />
-      <Card className="w-full rounded-lg p-5 space-y-3">
-        <Badge variant={"outline"}>High</Badge>
-        <div className="">
-          <p className="font-bold">This is my Task title.</p>
-          <p className="font-normal">
-            I have a very short description that has limits...
-          </p>
+      <Card className="flex w-full items-center rounded-lg p-5">
+        <div className="w-full space-y-3">
+          <div className="space-x-2">
+            <Badge variant={"outline"}>High</Badge>
+            <Badge>Personal</Badge>
+            <Badge>Project</Badge>
+          </div>
+          <div className="">
+            <p className="font-bold">This is my Task title.</p>
+            <p className="font-normal">
+              I have a very short description that has limits...
+            </p>
+          </div>
         </div>
-        <div className="space-x-2">
-          <Badge>Personal</Badge>
-          <Badge>Project</Badge>
-        </div>
+        <RiMore2Fill />
       </Card>
     </div>
   );
