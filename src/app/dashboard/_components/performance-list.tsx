57898167e17085c578fdf0980d887ef547479c85
jethrocadang <116892945+jethrocadang@users.noreply.github.com>
2024-07-02 20:18:01 +0800

    dashboard:[added Team Performance]

---
+++ src/app/dashboard/_components/performance-list.tsx
@@ -0,0 +1,81 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+
+type User = {
+  id: number;
+  name: string;
+  email: string;
+  tasksCompleted: number;
+};
+
+
+const TeamPerformance = () => {
+    const users: User[] = [
+      {
+        id: 1,
+        name: "Alice Johnson",
+        email: "alice.johnson@example.com",
+        tasksCompleted: 5,
+      },
+      {
+        id: 2,
+        name: "Bob Smith",
+        email: "bob.smith@example.com",
+        tasksCompleted: 7,
+      },
+      {
+        id: 3,
+        name: "Carol Williams",
+        email: "carol.williams@example.com",
+        tasksCompleted: 10,
+      },
+      {
+        id: 4,
+        name: "David Brown",
+        email: "david.brown@example.com",
+        tasksCompleted: 8,
+      },
+      {
+        id: 5,
+        name: "Eva Davis",
+        email: "eva.davis@example.com",
+        tasksCompleted: 6,
+      },
+    ];
+  return (
+    <Card className="grow ">
+      <CardHeader>
+        <CardTitle>Team Performance</CardTitle>
+        <CardDescription>
+          Your team finished 20 tasks this week.
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="gap-2.5">
+      {users.map((user) => (
+          <div key={user.id} className="flex w-full items-center justify-between px-2.5 mb-2">
+            <div className="flex items-center gap-2.5">
+              <Avatar>
+                <AvatarImage />
+                <AvatarFallback>{user.name[0]}{user.name.split(" ")[1][0]}</AvatarFallback>
+              </Avatar>
+              <div className="leading-4">
+                <p>{user.name}</p>
+                <p className="text-xs">{user.email}</p>
+              </div>
+            </div>
+            <p>{user.tasksCompleted} Tasks</p>
+          </div>
+        ))}
+      </CardContent>
+    </Card>
+  );
+};
+
+export default TeamPerformance;
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,15 @@
 import Cards from "./_components/cards";
 import Chart from "./_components/bar-chart";
+import TeamPerformance from "./_components/performance-list";
 
 const HomePage = () => {
   return (
     <div className="h-full w-full p-2.5">
       <Cards />
-      <div>
+      <div className="flex mt-2.5 gap-2.5"> 
         <Chart />
+        <TeamPerformance/>
+        
       </div>
     </div>
   );
